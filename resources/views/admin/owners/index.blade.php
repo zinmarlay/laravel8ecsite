4d7deaaa10e7df3a6f429349e0aa1a4b8ac0d97b
ZinMarLwin <zimmar07@gmail.com>
2023-08-20 12:48:43 +0900

    admin_toaster

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -10,8 +10,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font">
-                        <div class="container px-5 mx-auto">
+                        <div class="container px-5 mx-auto">                             
                           <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                            {{-- <x-flash-message status= "error" />   --}}
+                            @if(session('message'))
+                                <div class="bg-blue-300 w-1/2 mx-auto p-2 text-white">
+                                {{ session('message' )}} 
+                                </div>
+                            @endif
                             <div class="flex justify-end mb-4">
                               <button onclick="location.href=' {{route('admin.owners.create')}} '" class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">新規登録する</button>
                             </div>
