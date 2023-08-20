fd8e89a1777a633927fafc6e8d8dd2ee5bc1d5af
ZinMarLwin <zimmar07@gmail.com>
2023-08-20 13:33:08 +0900

    admin edit/update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -24,7 +24,7 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $owners = Owner::select('name','email','created_at')->get();
+        $owners = Owner::select('id','name','email','created_at')->get();
         return view('admin.owners.index', compact('owners'));
 
     }
@@ -84,7 +84,8 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        return view('admin.owners.edit',compact('owner'));
     }
 
     /**
@@ -96,7 +97,13 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+        return redirect()->route('admin.owners.index')
+            ->with('message','オーナー情報を更新しました。');
     }
 
     /**
